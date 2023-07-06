0637a1562b36e0b7166e6f7c68319ea505b20687
aixvision <138740065+aixvision@users.noreply.github.com>
2023-07-06 12:30:31 +0800

    x

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const result = await fetch("https://ip.me/", {
     "headers": {
         "User-Agent": "curl/7.79.1"
     }
-}).then((res) => res.text)
+}).then((res) => res.text())
 
 console.log(result);
